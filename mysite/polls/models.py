21de61c8a6f59669861f0ae60938338e573a2ac4
Ramnath Sharma <ramsharma.axel@gmail.com>
2021-03-13 23:54:30 +0530

    enhancing users model

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -2,7 +2,7 @@ import datetime
 
 from django.db import models
 from django.utils import timezone
-
+from django.contrib.auth.models import User
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -27,3 +27,6 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
